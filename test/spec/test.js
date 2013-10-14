7dcd8202d3be4c862375aaf1b8b6d056084647ee
Brandon Miller <brandonevanmiller@gmail.com>
2013-10-14 11:41:11 -0400

    New feature

--- test/spec/test.js
+++ test/spec/test.js
@@ -4,8 +4,14 @@
     describe('Give it some context', function () {
         describe('maybe a bit more context here', function () {
             it('should run here few assertions', function () {
-
+            	expect(true).to.equal(false)
             });
+
         });
     });
+    describe('More stuff', function() {
+    	 it('blarg', function() {
+            	expect(6).to.equal(6)
+            })
+    })
 })();
